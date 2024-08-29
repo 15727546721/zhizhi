d1b956b176b85c02d086e4317e611c0d3a52152a
xuzhipeng <xuzp@channelsoft.com>
2024-08-29 16:05:57 +0800

    fix: 解决跨域问题

---
+++ zhizhi-app/src/main/java/cn/xu/config/WebConfig.java
@@ -0,0 +1,20 @@
+package cn.xu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9527")  // 允许的域名
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 允许的请求方法
+                .allowedHeaders("*")  // 允许的请求头
+                .allowCredentials(true);  // 允许携带凭证
+    }
+}
+
--- zhizhi-app/src/test/java/cn/xu/test/ApiTest.java
+++ zhizhi-app/src/test/java/cn/xu/test/ApiTest.java
@@ -1,5 +1,6 @@
 package cn.xu.test;
 
+import cn.dev33.satoken.secure.SaSecureUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,8 @@ public class ApiTest {
     @Test
     public void test() {
         log.info("测试完成");
+        String sha256 = SaSecureUtil.sha256("123456");
+        log.info("sha256: {}", sha256);
     }
 
 }
