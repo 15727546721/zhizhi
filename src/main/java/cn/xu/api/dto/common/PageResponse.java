ac84841d1b776313252e44a4bab2e60392169acb
xuzhipeng <xuzp@channelsoft.com>
2025-01-17 17:56:46 +0800

    modify: 调整分页请求公共类和分页响应公共类

--- src/main/java/cn/xu/api/dto/common/PageResponse.java
+++ src/main/java/cn/xu/api/dto/common/PageResponse.java
@@ -1,47 +1,35 @@
 package cn.xu.api.dto.common;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.experimental.SuperBuilder;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Data
-@SuperBuilder
-@Schema(description = "分页响应对象")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "分页响应")
 public class PageResponse<T> {
-    
     @Schema(description = "当前页码")
-    private Integer pageNum;
-    
+    private Integer pageNo;
+
     @Schema(description = "每页数量")
     private Integer pageSize;
-    
+
     @Schema(description = "总记录数")
     private Long total;
-    
-    @Schema(description = "总页数")
-    private Integer pages;
-    
-    @Schema(description = "分页数据")
-    private T records;
-    
-    @Schema(description = "是否有下一页")
-    private Boolean hasNext;
-    
-    @Schema(description = "是否有上一页")
-    private Boolean hasPrevious;
-    
-    public static <T> PageResponse<T> of(Integer pageNum, Integer pageSize, Long total, T records) {
-        int pages = (int) Math.ceil((double) total / pageSize);
+
+    @Schema(description = "数据列表")
+    private T data;
+
+    public static <T> PageResponse<T> of(Integer pageNo, Integer pageSize, Long total, T data) {
         return PageResponse.<T>builder()
-                .pageNum(pageNum)
+                .pageNo(pageNo)
                 .pageSize(pageSize)
                 .total(total)
-                .pages(pages)
-                .records(records)
-                .hasNext(pageNum < pages)
-                .hasPrevious(pageNum > 1)
+                .data(data)
                 .build();
     }
 }
